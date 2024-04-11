aedfb59c024183537bf18088d57ecce468921b85
Leo Kuboschek <leo@kuboschek.me>
2024-04-11 22:13:31 +0200

    Add user menu

--- client/src/lib/components/UserMenu.svelte
+++ client/src/lib/components/UserMenu.svelte
@@ -1,22 +1,89 @@
 <script lang="ts">
-	import { Icon, UserCircle } from 'svelte-hero-icons';
+	import {
+		ArrowRightEndOnRectangle,
+		ArrowRightStartOnRectangle,
+		Bars3,
+		Cog,
+		Icon,
+		Plus
+	} from 'svelte-hero-icons';
 
+	import { goto } from '$app/navigation';
+	import { FiltersService, VotesService } from '$lib';
 	import { signIn, signOut, signedIn, userName } from '$lib/components/AuthContext.svelte';
 
-	async function handleUserMenuButtonClick() {
-		if ($signedIn) {
-			await signOut();
-		} else {
-			await signIn();
+	let menuOpen = false;
+
+	async function toggleMenu() {
+		menuOpen = !menuOpen;
+	}
+
+	async function checkMenuKey(event: KeyboardEvent) {
+		if (event.key === 'Space') {
+			menuOpen = false;
 		}
+	}
+
+	async function createVote() {
+		const vote = await VotesService.createVoteVotePost();
+
+		if (vote) {
+			goto(`/vote/${vote.vote._id}/edit`);
+		}
+		return vote;
+	}
+
+	async function createFilter() {
+		const filter = await FiltersService.createFilterFilterPost();
+
+		if (filter) {
+			goto(`/filter/${filter._id}`);
+		}
+		return filter;
 	}
 </script>
 
-<button class="btn" on:click={handleUserMenuButtonClick}>
-	<span><Icon src={UserCircle} class="h-6 w-6" /></span>
+<div class="relative">
 	{#if $signedIn}
-		<span>{$userName}</span>
+		<button class="btn" on:click={toggleMenu}>
+			<span>{$userName}</span>
+			<span><Icon src={Bars3} class="h-6 w-6" /></span>
+		</button>
 	{:else}
-		<span>Sign In</span>
+		<button class="btn" on:click={signIn}>
+			<span>Log In</span>
+			<span><Icon src={ArrowRightEndOnRectangle} class="h-6 w-6" /></span>
+		</button>
 	{/if}
-</button>
+	{#if menuOpen}
+		<div
+			class="btn-group-vertical variant-filled absolute right-0 top-full z-10 flex items-end rounded-md p-2"
+			on:click={toggleMenu}
+			on:keypress={checkMenuKey}
+			role="menu"
+			tabindex="0"
+		>
+			<button on:click={createVote} type="button" class="group">
+				<span>Create Vote</span>
+				<span><Icon src={Plus} class="group-hover:text-success-500 h-6 w-6" /></span>
+			</button>
+			<button on:click={createFilter} type="button" class="group">
+				<span>Create Filter</span>
+				<span><Icon src={Plus} class="group-hover:text-success-500 h-6 w-6" /></span>
+			</button>
+			<button on:click={toggleMenu} type="button" class="group">
+				<span>Settings</span>
+				<span><Icon src={Cog} class="group-hover:text-primary-500 h-6 w-6" /></span>
+			</button>
+			<button on:click={signOut} on:click={toggleMenu} type="button" class="group">
+				<span>Log Out</span>
+				<span
+					><Icon
+						src={ArrowRightStartOnRectangle}
+						class="group-hover:text-error-500 h-6 w-6"
+					/></span
+				>
+			</button>
+		</div>
+	{/if}
+</div>
