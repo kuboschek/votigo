d3af2f512163b7a7622a9340e32ec8aa3a6fea6d
Leo Kuboschek <leo@kuboschek.me>
2024-04-28 16:07:53 +0200

    Fix test

--- client/tests/test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('button', { name: 'Create Vote' })).toBeVisible();
-	await expect(page.getByRole('button', { name: 'Create Filter' })).toBeVisible();
-});
